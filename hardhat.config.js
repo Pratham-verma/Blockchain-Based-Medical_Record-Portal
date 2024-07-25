1e09eef578a5b6e142fda4d90c4cf468363af180
Pratham verma <vermapratham623@gmail.com>
2024-07-25 13:39:31 +0530

    some modification

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,17 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      accounts: {
+        count: 10,
+        accountsBalance: "10000000000000000000000" // 10000 ETH
+      }
+    },
+    localhost: {
+      chainId: 31337,
+      url: "http://127.0.0.1:8545",
+    }
+  }
 };
